1749a01ffa4a1aecbe2b2a6f48e0a1e55ad92bc2
Dave E <david.craciunescu@edu.uah.es>
2019-10-12 12:22:24 +0200

    Implement packrat in project

--- LAB/PL1/PL1.R
+++ LAB/PL1/PL1.R
@@ -9,78 +9,28 @@
 ################################################################################
 
 # Load needed packages.
-library(modeest)
+library(foreign)
 
-# Check there is a file to load.
-while (!"satelites.txt" %in% list.files(getwd()))
+# Check if files are in directory.
+
+needed <- c("cardata.sav", "satelites.txt", "calc.R")
+
+while(!needed %in% list.files(getwd()))
 {
-    print("Data file not found. Add \"satelites.txt\" to the current directory.")
+    print("Files missing!. Check cardata, satelites and calc are in your dir")
     invisible(readline(prompt="Press [enter] to continue"))
 }
 
-# Load the data of the "satellites.txt"
-satelites <- read.table("satelites.txt")
+# Load the data of the files.
+satelites   <- read.table("satelites.txt")
+cardata     <- read.spss("cardata.sav", to.data.frame = T)
 
-################################################################################
+# Access info.
+radio       <- (satelites $ radio)[!is.na(radio)]
+mpg         <- (cardata $ mpg)[!is.na(mpg)]
 
-# 1. Calculate absolute and relative satellite radius frequencies.
+# Source operations.
+source("calc.R")
 
-## 1.1. Absolute frequencies.
-absoluteFreq    <- function(set) {table(set)}
-cumAbsoluteFreq <- function(set) {cumsum(absoluteFreq(set))}
+print("Test is %i", absoluteFreq(radio))
 
-## 1.2. Relative frequencies.
-   relativeFreq <- function(set) {table(set) / length(set)}
-cumRelativeFreq <- function(set) {cumsum(relativeFreq(set))}
-
-################################################################################
-
-# 2. Arithmetic mean.
-arithmeticMean <- function(set, usrTrim = 0) (mean(set, trim = usrTrim))
-
-################################################################################
-
-# 3. Measures of dispersion.
-
-# The following page was used as a reference for this section.
-# http://iridl.ldeo.columbia.edu/dochelp/StatTutorial/Dispersion/index.html#Intro
-
-## 3.1. Range.
-          range <- function(set) {max(set) - min(set)}
-
-## 3.2. Standard Deviation.
-   stdDeviation <- function(set) 
-{
-   sd(set) * (sqrt((length(set) - 1) / length(set)))
-}
-
-## 3.3. Variance.
-       variance <- function(set) {var(set) * (length(set) - 1 / length(set))} 
-
-## 3.4. Root Mean Square.
-    rootMeanSqr <- function(set) {sqrt(mean(set ^ 2))}
-
-## 3.5. Root Mean Square Anomaly.
-  rootMeanSqrAn <- function(set) {sqrt(sum(set - mean(set)) ^ 2) / length(set)}
-
-## 3.6. Interquartile Range.
-interQuartRange <- function(set) {IQR(set)}
-
-## 3.7. Median Absolute Deviation.
-medAbsDeviation <- function(set) {mad(set)}
-
-################################################################################
-
-# 4. Measures of order.
-
-## 4.1. Median.
-getMedian    <- function(set) {median(set)}
-
-## 4.2. Mode.
-getMode      <- function(set) {mfv(set)}
-
-## 4.2. Quartiles.
-getQuartiles <- function(set) {quantile(set)}
-
-## 4.3. 54th Quantile.
-getQuantiles <- function(set, range = 0) {quantile(set, probs = range)}
