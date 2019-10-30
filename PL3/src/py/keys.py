13bb896aee6921737e0ce6c2d8cc1fd03df46152
Dave E <david.craciunescu@edu.uah.es>
2019-10-30 07:34:45 +0100

    Me voy a clase

---
+++ PL3/src/py/keys.py
@@ -0,0 +1,7 @@
+# Private access keys for davidmrchappy@gmail.com
+# DO NOT UPLOAD OR SHARE
+
+consumer_key        = "Md6UeqRZJNUwwtsGQoqH5jkAM"
+consumer_secret     = "PVeg4D3re7d8qcw1kDQ4QURRtUFbK8agVOUmxcIAekpuudx8rD"
+access_token        = "1189159169183621120-EWMEbYfP2N75uIlwERwkjRH4gchCm0"
+access_token_secret = 'tw9q0wR3e9IAdXhzn4Ryp7Pv9o9ArCpov94rsmxNMTmHK'
